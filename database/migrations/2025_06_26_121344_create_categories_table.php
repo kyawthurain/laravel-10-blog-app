3fe379d5dbb751499b8775861d911b1aae692629
kyawthurain <kyawthurain1982002@gmail.com>
2025-06-26 18:57:29 +0630

    migrations user-post-tag-category-profile

--- database/migrations/2025_06_26_121344_create_categories_table.php
+++ database/migrations/2025_06_26_121344_create_categories_table.php
@@ -11,15 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role',['owner','admin','user'])->default('user');
-            $table->foreignId('profile_id');
-            $table->rememberToken();
+            $table->foreignId('user_id');
+            $table->string('slug');
             $table->timestamps();
         });
     }
@@ -29,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
