322cd1f1e5d23396de184f340f7cd20405dd107e
kyawthurain <kyawthurain1982002@gmail.com>
2025-07-03 17:09:22 +0630

    layouts done

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <h1>Test</h1>
+        </div>
+    </div>
+</div>
+
+@endsection
